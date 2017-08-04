fe8bd8473f532755b792fe14b0b330f98c4eb095
labarrett22 <labarrett22@gmail.com>
2017-08-03 20:01:30 -0700

    Added fields to Member model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804021310) do
+ActiveRecord::Schema.define(version: 20170804024311) do
 
   create_table "members", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "bio"
   end
 
 end
