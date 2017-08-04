a350135b9abe30a7a9130b832b6ed61741014b7b
labarrett22 <labarrett22@gmail.com>
2017-08-03 22:59:41 -0700

    Added field descriptions to views

--- db/migrate/20170804023601_add_user_name_to_members.rb
+++ db/migrate/20170804023601_add_user_name_to_members.rb
@@ -4,6 +4,5 @@ class AddUserNameToMembers < ActiveRecord::Migration[5.1]
     add_column :members, :firstname, :string
     add_column :members, :lastname, :string
     add_column :members, :email, :string
-    
   end
 end
