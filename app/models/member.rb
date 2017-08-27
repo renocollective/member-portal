88377ec21f3824e037792e6cda74f6d634fa3828
Phil Cohen <github@phlippers.net>
2017-08-27 00:30:57 -0700

    Add Rubocop to CI
    Fix Rubocop violations and add to test suite.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
+# ActiveRecord Member user
 class Member < ApplicationRecord
-   mount_uploader :avatar, AvatarUploader
-   validates :username, :firstname, :lastname, :email, :bio, :presence => true
+  mount_uploader :avatar, AvatarUploader
+  validates :username, :firstname, :lastname, :email, :bio, presence: true
 end
