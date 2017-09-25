dc3a65ce15c8ade5d51dba6de903c1480d190fa7
BrendaPenn <brendap@nevada.unr.edu>
2017-09-25 14:09:03 -0700

    Added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Added from devise instructions
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
