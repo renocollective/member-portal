40885c15d3c40960d5f4477d85043a07e35cbbb4
Phil Cohen <github@phlippers.net>
2017-10-12 22:25:49 -0700

    Configure `simplecov`
    * Configure for Rails
    * Ignore dependencies from Bundler and vendor/

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'bundle|vendor'
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
