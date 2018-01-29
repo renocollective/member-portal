06b3a3f18909ff09461648df7bfe8a380027d7f1
BrendaPenn <brendap@nevada.unr.edu>
2018-01-29 13:23:38 -0800

    Change after confirmation path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :members, controllers: { confirmations: 'confirmations' }
+  devise_for :members
 
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
