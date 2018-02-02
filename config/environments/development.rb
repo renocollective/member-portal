590e4183d0176716df7415b8a97d554453d24845
Josh <go@revoltmedia.com>
2018-02-01 20:27:21 -0800

    moved env variables to .env.local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+  config.web_console.whitelisted_ips = '172.18.0.1'
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
