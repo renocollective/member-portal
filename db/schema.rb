365980e4d9cf26e6fc6d27a029e1db278a4b4a5a
BrendaPenn <brendap@nevada.unr.edu>
2018-02-12 13:29:51 -0800

    Add admin attribute to member model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213043044) do
+ActiveRecord::Schema.define(version: 20180212204134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180213043044) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "company"
     t.string "website_name"
     t.string "website_url"
@@ -49,21 +50,9 @@ ActiveRecord::Schema.define(version: 20180213043044) do
     t.string "linkedin"
     t.string "instagram"
     t.string "slack"
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer "invitation_limit"
-    t.string "invited_by_type"
-    t.bigint "invited_by_id"
-    t.integer "invitations_count", default: 0
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_members_on_confirmation_token", unique: true
     t.index ["email"], name: "index_members_on_email", unique: true
-    t.index ["invitation_token"], name: "index_members_on_invitation_token", unique: true
-    t.index ["invitations_count"], name: "index_members_on_invitations_count"
-    t.index ["invited_by_id"], name: "index_members_on_invited_by_id"
-    t.index ["invited_by_type", "invited_by_id"], name: "index_members_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
-
 end
