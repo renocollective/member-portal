c17ea70e88810efe42568ceb09df4d49cd029d33
BrendaPenn <brendap@nevada.unr.edu>
2018-02-14 11:05:25 -0800

    Rebase with mailer and devise invitations

--- db/migrate/20180212204134_add_admin_to_members.rb
+++ db/migrate/20180212204134_add_admin_to_members.rb
@@ -1,9 +1,9 @@
 class AddAdminToMembers < ActiveRecord::Migration[5.1]
   def change
     if column_exists? :members, :admin
-                remove_column :members, :admin, :boolean, default: false
+      remove_column :members, :admin, :boolean, default: false
     end
-    
+
     add_column :members, :admin, :boolean, default: false
   end
 end
