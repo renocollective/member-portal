c2bd3ab595a31d33e0b770f71e6a0e508c178942
BrendaPenn <brendap@nevada.unr.edu>
2018-03-05 12:53:44 -0800

    add post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,12 @@
 class PostsController < ApplicationController
   before_action :find_post, only: %i[show edit update destroy]
   def index
-    @posts = Post.all
+    if params[:category].blank?
+    @posts = Post.all.order('created_at DESC')
+    else
+    @category_id = Category.find_by(name: params[:category]).id
+    @posts = Post.where(category_id: @category_id).order('created_at DESC')
+    end
   end
 
   def show() end
