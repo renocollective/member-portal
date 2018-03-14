93fa2b822ea98b528a99273e7d0fe1621147608f
Jackson Melcher <jacksonmelcher@protonmail.com>
2018-03-14 14:27:14 -0700

    Styled homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :homepage
+  root to: 'homepage#index'
   namespace :admin do
     resources :members
 
---
+++ test/controllers/homepage_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HomepageControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
