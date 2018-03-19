2ae4e475d4cfecad61456ca01327aa1d085f7be8
BrendaPenn <brendap@nevada.unr.edu>
2018-03-19 10:40:11 -0700

    Modiy views for comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,28 @@
 # comments for posts controller
 class CommentsController < ApplicationController
   before_action :find_post
+  before_action :find_comment, only: %i[edit update destroy]
 
   def create
     @comment = @post.comments.create(comment_params)
     @comment.member_id = current_member.id
     @comment.save
+    redirect_to post_path(@post)
+  end
 
-    if @comment.save
+  def edit() end
+
+  def update
+    if @comment.update(comment_params)
       redirect_to post_path(@post)
     else
-      redirect_to :new
-    end  
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
   private
@@ -26,5 +37,9 @@ class CommentsController < ApplicationController
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def find_comment
+    @comment = @post.comments.find(params[:id])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: AvatarUploader
+
+# ActiveRecord comment model
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :member
