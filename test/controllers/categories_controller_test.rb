44a286757bd5ed2851204c0e33ef80c3d238ca2b
BrendaPenn <brendap@nevada.unr.edu>
2018-03-21 13:52:58 -0700

    Add tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,7 +3,30 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = create(:category)
+    @member = create(:member)
+    login_as(@member, 'scope' => :member)
+  end
+
+  test 'should create category' do
+    assert_difference('Category.count') do
+      post posts_url, params: {
+        category: {
+          name: 'Category'
+        }
+      }
+    end
+  end
+
+  test 'should get new' do
+    get new_category_url
+    assert_response :success
+  end
+
+  test 'should destroy category' do
+    assert_difference('Category.count', -1) do
+      delete category_url(@category)
+    end
+  end
 end
