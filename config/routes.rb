cf7853db404521909a8cffd7997b07ad8edca53d
BrendaPenn <brendap@nevada.unr.edu>
2018-04-02 12:18:56 -0700

    If not logged in, redirect to sign in page automatically

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
   namespace :admin do
     resources :members
     root to: 'members#index'
   end
 
-  authenticated :member do
-    devise_for :members
-    resources :members
-    root to: 'home#index'
+  devise_for :members
+  resources :members
+  root to: 'home#index'
+
+  resources :posts do
+    resources :comments
+    root to: 'posts#index'
   end
-
-  authenticated :member do
-    resources :posts do
-      resources :comments
-      root to: 'posts#index'
-    end
-  end
-
-  root "home#index"
-
 end
