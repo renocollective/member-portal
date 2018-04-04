a8c8ddec6925f5ab6661db3d8c74211d36d90bbf
BrendaPenn <brendap@nevada.unr.edu>
2018-04-04 15:18:12 -0700

    add announcement admin test and rubocop changes

--- spec/controllers/announcements_controller_spec.rb
+++ spec/controllers/announcements_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe AnnouncementsController, type: :controller do
-
 end
--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :announcement do
-    title "MyString"
-    content "MyText"
+    title 'MyString'
+    content 'MyText'
   end
 end
