4da8e70db7fac5bf77e3d5d2eae926ec2fa09d9a
Colin Loretz <colin@mechani.sm>
2018-04-06 12:20:24 -0700

    Added title to member model and more UI work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,9 @@
 class HomeController < ApplicationController
   before_action :check_login, only: %i[index]
   def index
+    @new_conversation = Post.new
     @conversations = Post.order(created_at: :desc).last(10)
+    @announcements = Announcement.order(created_at: :desc).last(3)
   end
 
   def check_login
