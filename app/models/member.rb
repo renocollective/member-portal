4d81826ee669c714ef7c63633f968696829a7166
BrendaPenn <brendap@nevada.unr.edu>
2018-04-09 12:37:11 -0700

    delete comments and posts when deleting member

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,8 +2,8 @@
 
 # ActiveRecord Member user
 class Member < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :confirmable, :database_authenticatable, :registerable,
