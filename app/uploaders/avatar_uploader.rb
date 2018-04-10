adb169b32ef11f6212d9db035f670aeb1c98ad9b
BrendaPenn <brendap@nevada.unr.edu>
2018-04-10 11:44:07 -0700

    Set default avatar url

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,4 +15,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def default_url
+    '/fallback/' + [version_name, 'default-avatar.jpeg'].compact.join('_')
+  end
 end
