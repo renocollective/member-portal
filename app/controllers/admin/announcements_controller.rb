25d2ab3f59610636a383326663eb5546cc7ebd48
BrendaPenn <brendap@nevada.unr.edu>
2018-04-11 12:26:11 -0700

    undelete admin controller files

---
+++ app/controllers/admin/announcements_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Admin
+  # admin member dashboard controller
+  class AnnouncementsController < Admin::ApplicationController
+  end
+end
---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Admin
+  # admin member dashboard controller
+  class CategoriesController < Admin::ApplicationController
+  end
+end
